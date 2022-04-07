ab6a1bcf5d84aaa7bf22d03d282e81b746370e9d
Weiller Carvalho <engweiller@hotmail.com>
2022-04-07 10:37:47 -0300

    cssadd

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
+    
     return render(request, 'cadastro.html')
 def logar(request):
     return HttpResponse('Logar')
