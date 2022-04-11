6a602d838597b8ae6cf5f735097a45ac93251036
Weiller Carvalho <engweiller@hotmail.com>
2022-04-11 12:57:03 -0300

    finishedwindowfindjobs

--- jobs/views.py
+++ jobs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Jobs
 from datetime import datetime
 
@@ -42,3 +42,10 @@ def encontrar_jobs(request):
             jobs = Jobs.objects.filter(reservado=False)
 
         return render(request, 'encontrar_jobs.html', {'jobs': jobs})
+
+def aceitar_job(request, id):
+    job = Jobs.objects.get(id=id)
+    job.profissional = request.user
+    job.reservado = True
+    job.save()
+    return redirect('/jobs/encontrar_jobs')
